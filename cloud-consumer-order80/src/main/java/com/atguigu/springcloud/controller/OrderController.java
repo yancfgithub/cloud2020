46891637b0b5f7b60dd0d2af5fc490b29b75a54f
zhangsan <zhangsan@126.com>
2020-12-25 14:11:04 +0800

    实现负载均衡轮询

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -3,13 +3,19 @@ package com.atguigu.springcloud.controller;
 
 import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
+import com.atguigu.springcloud.lb.MyLoadBalance;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -21,8 +27,14 @@ public class OrderController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @Autowired
+    private MyLoadBalance myLoadBalance;
+
     /**
-     * 从8001中调用创建方法
+     * 调用8001中的创建方法
      * @param payment
      * @return
      */
@@ -35,5 +47,18 @@ public class OrderController {
     public CommonResult<Payment> getPayment(@PathVariable("id")Long id) {
         return restTemplate.getForObject(PAYMENT_URL+"/payment/get/"+id,CommonResult.class);
 
+    }
+
+    @GetMapping(value = "/consumer/payment/lb")
+    public String getLoadBalance() {
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        if(null == instances || instances.size() <= 0) {
+            return null;
+        }
+
+        ServiceInstance serviceInstance = myLoadBalance.instances(instances);
+        URI uri = serviceInstance.getUri();
+        System.out.println("uri:"+uri);
+        return restTemplate.getForObject(uri+"/payment/lb",String.class);
     }
 }
--- cloud-provider-payment8002/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8002/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -51,4 +51,9 @@ public class PaymentController {
             return new CommonResult(500,"新增数据失败",null);
         }
     }
+
+    @GetMapping(value = "/payment/lb")
+    public String getPaymentLB() {
+        return serverPort;
+    }
 }
